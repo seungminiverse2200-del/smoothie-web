4b0f73e414a8b355f8d8859cdd3ee4bdfac64504
seungminiverse2200-del <seungminiverse2200@gmail.com>
2026-07-11 20:11:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function showPage(id) {
+
+
+    document.querySelectorAll(".page").forEach(p ==> {p.style.display = "none";
+    }
+
+    document.getElementById(id).style.display = "block";   
+}
+
+
+showPage("home");
+
+
+function buyProduct(n) {
+
+    alert("You Brought " + n + "!");
+    
+}
